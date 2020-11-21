fa5ffadded868035ae559b858e8ae0ca6ad436a0
Andre Hatushikano <andrehatushikano@gmail.com>
2020-11-21 16:18:47 -0300

    Started creating tests

--- LLS/test.py
+++ LLS/test.py
@@ -1,4 +1,4 @@
-import LLS
+from LLS import *
 # TB for LLS
 # Scenario based testing model. Create objects, write to them, open, close, read, write, etc...
 # Make sure we can create objs and put data into them. Use 4 byte cache model for testing.
@@ -6,7 +6,7 @@ import LLS
 # Test for invalid inputs (size is off etc.) to ensure it sends back msg to PMU/CH.
 # Test case for gain a msg to retire bast
 
-# FNS TO TEST
+# FNS LIST TO TEST
 # mapBASTtoDSAST(dsast, gast)
 # gastRequest(dsast)
 # writeToBAST(dsast)
@@ -20,8 +20,33 @@ import LLS
 # writeThrough(dsast, packet)
 # sendMsgRetire()							# dummy implementation
 # rcvOkToUnmap()							# dummy implementation
+
+def testFunc(func):
+	if(func):
+		print("Test {} success".format(func.__name__))
+	else:
+		print("Test {} failed".format(func.__name__))
+
+def testCreatingFile():
+	global sastBast
+	# create dsast to bast mapping
+	adsast = DSAST()
+	agast = GAST()
+	mapBASTtoDSAST(adsast, agast)
+	abast = bastTable[(agast.domain, agast.key)]
+	assert sastBast[adsast] == abast
+
+	# call create file to check if new bast is associated to given dsast
+	createFile(adsast)
+	assert type(sastBast[adsast]) == BAST
+	assert sastBast[adsast] != abast
+
+	return 1
+
+
 def main():
 
+	testFunc(testCreatingFile)
 
 
 if __name__ == "__main__":
