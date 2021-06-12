95aab6cac772b053aeab8276d2edc058f07c805c
prakasr1208 <prakasr1208@github.com>
2021-06-12 17:48:26 -0400

    made progress towards SNE CH connection

--- SNE/SNE.py
+++ SNE/SNE.py
@@ -1,67 +1,95 @@
-import socket
-import socket   
-# import thread module 
-from thread import *
-import threading    
-import random         
- #threading 
-# next create a socket object
-#dump the socket number into a named file (e.g what is lls socket number)
-#others read from it 
-#give all systems names, they open up sockets on bootup and write the socket numbers to files
-#each HW comp has 2 sockets (if datagram, then add header e.g if cache trans or message notific)
-print_lock = threading.Lock() 
+#SNE simulation @author: Raghul Prakash
+import threading
+from CH_param_sim import *
+import L4_cache
+from pwn import *
+from MN_queue import *
+import time
 
-s = socket.socket()          
-print "Socket successfully created"
-  
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = random.randint(2000,5000)       
-file_o = open("SNE.txt","w")              
-file_o.write("{}".format(port))
-file_o.close()  
-  
-# thread function 
-def threaded(c): 
-    while True: 
-  
-        # data received from client 
-        data = c.recv(1024) 
-        if not data: 
-            print('Bye') 
-              
-            # lock released on exit 
-            print_lock.release() 
-            break
-  
-    # connection closed 
-    c.close()  
-  
-  
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests  
-# coming from other computers on the network 
-s.bind(('', port))         
-print "socket binded to %s" %(port) 
-  
-# put the socket into listening mode 
-s.listen(5)      
-print "socket is listening"            
-  
-# a forever loop until we interrupt it or  
-# an error occurs 
-while True: 
-  
-   # Establish connection with client. 
-   c, addr = s.accept()      
-   print 'Got connection from', addr 
-   print_lock.acquire() 
-   # send a thank you message to the client.  
-   c.send('Thank you for connecting to SNE') 
-  
-   # Close the connection with the client 
-   start_new_thread(threaded, (c,)) 
-s.close() 
+MAX_SYS_SOCKET = 2
+CAST_PMU = 1
+CAST_SNE = 8
+CAST_SAL = 16
+READ = 0
+WRITE = 1
+global ACK
+
+#semaphores of each thread/socket/NAS
+ACK = [threading.Semaphore(value=1)] * MAX_SYS_SOCKET
+
+#message format: NPAST, CAST, NSAST
+"""
+SNE:
+the SNE is a network proxy that is managed by a separate embedded system. This containerization makes it more secure than
+running the networking stack the kernel.
+Currently supports IPC across two processes. Next, need to connect to external IP with HTTP methods
+"""
+class SNE:
+	"""
+	__init__:
+	Constructor that initializes the SNE. The SNE has an MN queue for message notifications indicating either setup/packet send/ teardown. It has a control table
+	that indicates which NAS is busy/active and a local L3 cache. It has multiple threads running per NAS that iteract with the TCP/IP stack
+	"""
+	def __init__(self):
+		self.MN_Queue = MN_queue() #Message Notification q ueue of the SNE
+		self.control_table = [0] * MAX_SYS_SOCKET #The state of the socket/NAS. is it active/idle.
+		self.CAST_SNE = 17 #The CAST/PID of the SNE
+		#self.L3_cache = L3cache() #The local L3 cache
+		self.system_sockets =[threading.Thread(thread_function,args=(self,NSAST),name=str(i)) for i in range(2 * MAX_SYS_SOCKET)] #2x because two address spaces. one for rx and tx
+		self.message = 0 #the current message that is being read by the SNE.
+		#Socket remote connection. MAX_SYS_SOCKET number of threads are being run and started by __init__. The argument passed is the socket
+		#handle.
+		#multiply MAX by 2 since there is two address spaces that are orthogonal. one for rx and the other for tx
+		for NSAST in range(2 * MAX_SYS_SOCKET):
+			socket_NSAST = remote('127.0.0.1', 3000 + NSAST)
+			ACK[NSAST].acquire()
+			self.system_sockets[NSAST].start(self, NSAST)
+		self.main()
+	"""
+	thread_function:
+	each socket/NSAST runs a parallel thread that receives a packet via the L3 cache from the SAL.
+	this packet is passed into the TCP/IP stack and a new packet coming from it (IP packet) is passed in to the NAS.
+	the NAS has a local copy in the L3 cache. the new packet is written into this address space.
+	"""
+	def thread_function(self, NSAST):
+		while (True):
+			ACK[NSAST].acquire()
+			buff = Cache_Hierarchy(READ,NSAST,None)		#read from the NAS address of the Cache Hierarchy. this is the packet coming from the PN via the SAL
+			self.system_sockets[NSAST].send(buff) #send this packet into the TCP/IP stack for it to become a TCP/IP packet
+			time.sleep(1) #wait for the packet to be sent into the stack
+			inv_NSAST = 3000 + MAX_SYS_SOCKET + NSAST #use the other orthogonal address space a.k.a invert it
+			buff_ip = self.system_sockets[inv_NSAST].recv(buff) #receive the ip packet from TCP/IP stack and pass it into a buffer
+			Cache_Hierarchy(WRITE,inv_NSAST,buff_ip)  #write this buffer into the Cache Hierarchy
+			ACK[NSAST].release()	
+	"""
+	main:
+	Needs to be called to start the SNE. This is the entry point. It first reads valid message from the MN queue.
+	Then checks if the message is a setup or a read/write.   
+	"""
+	def main(self):
+		while (True):
+			message = MN_decode(Cache_Hierarchy(READ,CAST_SNE, None)) #read the MN address space of SNE and decode the message
+			if (MN_decode(message).valid == True): # check if the message is valid
+				if (MN_decode(message).setup == True): #check if it is a setup message or an actual read/write message
+					for i in range(MAX_SYS_SOCKET): #find available socket and enable/activate it
+						if (self.control_table[MAX_SYS_SOCKET].enable == 0):
+							self.control_table[MAX_SYS_SOCKET].enable = 1
+							break
+						NSAST = i
+						self.control_table[i].NPAST = MN_decode(message).NPAST #the control table should also store the NPAST that is used for this socket
+						self.control_table[i].CAST = MN_decode(message).CAST #it should also store which CAST/PID this socket is attached to now
+					Cache_Hierarchy(WRITE,CAST_PMU,MN_encode(NSAST)) #it should send/write the NSAST that the SNE selected to the PMU
+				#setup is done
+				elif (MN_decode(message).read): #for read/write 
+					if MN_decode(message).read: #the message is a read and the message also contains the NSAST 
+						if (message.valid):
+							NSAST = MN_decode(message).NSAST
+							ACK[NSAST].release() #release the lock so that the thread can acquire in an run its code
+					#writes are tougher and more complicated. needs more work for writes
+					#else if MN_decode(message).write:
+					#	Cache_Hierarchy(WRITE,MN_encode(NSAST, CAST_SNE, NPAST) + ,None)	
+					#	Cache_Hierarchy(WRITE,MN_decode(message).NSAST,None)								
+
+
+if (__name__ == "__main__"):
+	SNE = SNE()
--- SNE/sim_connectSNEtoCH.py
+++ SNE/sim_connectSNEtoCH.py
@@ -209,10 +209,100 @@ miss = []
 hits = []
 global_dsast = DSAST()
 mapBASTtoDSAST(global_dsast)
+#SNE#
+from SNE import *
+MAX_SYS_SOCKET = 2
+CAST_PMU = 1
+CAST_SNE = 8
+CAST_SAL = 16
+READ = 0
+WRITE = 1
+global ACK
+#semaphores of each thread/socket/NAS
+#semaphores of each thread/socket/NAS
+ACK = [threading.Semaphore(value=1)] * MAX_SYS_SOCKET
+
+#message format: NPAST, CAST, NSAST
+"""
+SNE:
+the SNE is a network proxy that is managed by a separate embedded system. This containerization makes it more secure than
+running the networking stack the kernel.
+Currently supports IPC across two processes. Next, need to connect to external IP with HTTP methods
+"""
+class SNE:
+	"""
+	__init__:
+	Constructor that initializes the SNE. The SNE has an MN queue for message notifications indicating either setup/packet send/ teardown. It has a control table
+	that indicates which NAS is busy/active and a local L3 cache. It has multiple threads running per NAS that iteract with the TCP/IP stack
+	"""
+	def __init__(self):
+		self.MN_Queue = MN_queue() #Message Notification q ueue of the SNE
+		self.control_table = [0] * MAX_SYS_SOCKET #The state of the socket/NAS. is it active/idle.
+		self.CAST_SNE = 17 #The CAST/PID of the SNE
+		#self.L3_cache = L3cache() #The local L3 cache
+		self.system_sockets =[threading.Thread(thread_function,args=(self,NSAST),name=str(i)) for i in range(2 * MAX_SYS_SOCKET)] #2x because two address spaces. one for rx and tx
+		self.message = 0 #the current message that is being read by the SNE.
+		#Socket remote connection. MAX_SYS_SOCKET number of threads are being run and started by __init__. The argument passed is the socket
+		#handle.
+		#multiply MAX by 2 since there is two address spaces that are orthogonal. one for rx and the other for tx
+		for NSAST in range(2 * MAX_SYS_SOCKET):
+			socket_NSAST = remote('127.0.0.1', 3000 + NSAST)
+			ACK[NSAST].acquire()
+			self.system_sockets[NSAST].start(self, NSAST)
+		self.main()
+	"""
+	thread_function:
+	each socket/NSAST runs a parallel thread that receives a packet via the L3 cache from the SAL.
+	this packet is passed into the TCP/IP stack and a new packet coming from it (IP packet) is passed in to the NAS.
+	the NAS has a local copy in the L3 cache. the new packet is written into this address space.
+	"""
+	def thread_function(self, NSAST):
+		while (True):
+			ACK[NSAST].acquire()
+			#buff = cache.get(NSAST << )
+			buff = Cache_Hierarchy(READ,NSAST,None)		#read from the NAS address of the Cache Hierarchy. this is the packet coming from the PN via the SAL
+			self.system_sockets[NSAST].send(buff) #send this packet into the TCP/IP stack for it to become a TCP/IP packet
+			time.sleep(1) #wait for the packet to be sent into the stack
+			inv_NSAST = 3000 + MAX_SYS_SOCKET + NSAST #use the other orthogonal address space a.k.a invert it
+			buff_ip = self.system_sockets[inv_NSAST].recv(buff) #receive the ip packet from TCP/IP stack and pass it into a buffer
+			#cache.put(NSAST << ,buff_ip)
+			Cache_Hierarchy(WRITE,inv_NSAST,buff_ip)  #write this buffer into the Cache Hierarchy
+			ACK[NSAST].release()	
+	"""
+	main:
+	Needs to be called to start the SNE. This is the entry point. It first reads valid message from the MN queue.
+	Then checks if the message is a setup or a read/write.   
+	"""
+	def main(self):
+		while (True):
+			#message = MN_decode(cache.get(CAST_SNE << 2))
+			message = MN_decode(Cache_Hierarchy(READ,CAST_SNE, None)) #read the MN address space of SNE and decode the message
+			if (MN_decode(message).valid == True): # check if the message is valid
+				if (MN_decode(message).setup == True): #check if it is a setup message or an actual read/write message
+					for i in range(MAX_SYS_SOCKET): #find available socket and enable/activate it
+						if (self.control_table[MAX_SYS_SOCKET].enable == 0):
+							self.control_table[MAX_SYS_SOCKET].enable = 1
+							break
+						NSAST = i
+						self.control_table[i].NPAST = MN_decode(message).NPAST #the control table should also store the NPAST that is used for this socket
+						self.control_table[i].CAST = MN_decode(message).CAST #it should also store which CAST/PID this socket is attached to now
+					#cache.put(CAST_PMU << 2, MN_encode(NSAST))
+					Cache_Hierarchy(WRITE,CAST_PMU,MN_encode(NSAST)) #it should send/write the NSAST that the SNE selected to the PMU
+				#setup is done
+				elif (MN_decode(message).read): #for read/write 
+					if MN_decode(message).read: #the message is a read and the message also contains the NSAST 
+						if (message.valid):
+							NSAST = MN_decode(message).NSAST
+							ACK[NSAST].release() #release the lock so that the thread can acquire in an run its code
+					#writes are tougher and more complicated. needs more work for writes
+					#else if MN_decode(message).write:
+					#	Cache_Hierarchy(WRITE,MN_encode(NSAST, CAST_SNE, NPAST) + ,None)	
+					#	Cache_Hierarchy(WRITE,MN_decode(message).NSAST,None)				
+#SNE#
 def main():
     createFiles(300)
     initFiles(300, 80000)
-
+	SNE()
     global caches
     for i in range(5):
         caches.append(cache(i))
